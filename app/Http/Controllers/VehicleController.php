1e5f2271c120626f7f6bba21ff833f13a198205a
darkorakic <darko.rakic@digitialinsurcance.io>
2023-04-24 18:44:13 +0200

    Added api call,cache, basic view

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers;
 
 
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+
 class VehicleController extends Controller
 {
     public function getVehicle($id){
 
+        try {
+            $url = 'https://www.classic-trader.com/api/vehicle-ad/'.$id;
+
+            if (Cache::has('vehicle-'.$id)) {
+                $vehicle = Cache::get('vehicle-'.$id);
+            } else {
+                $client = new Client();
+
+                 $response = $client->request('GET', $url);
+
+                $json = json_decode($response->getBody()->getContents());
+                $vehicle = $json->data;
+
+                Cache::put('vehicle-'.$id,$vehicle,1000);
+            }
+
+
+            return view('Vehicle.show')->with([
+               'vehicle' => $vehicle
+            ]);
+        }catch (\Exception $e){
+            Log::error($e->getMessage());
+
+            return response('Item not found!');
+        }
     }
 }
